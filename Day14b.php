ff9e52913001dcd7d3d6f352337121c20f7f2abb
Rien <Rien@RIENPC>
2019-01-14 22:11:25 +0000

    Day 14!!

--- Day14b.php
+++ Day14b.php
@@ -15,16 +15,20 @@ $tempKey = '';
 $foundKey = '';
 $tempi = 0;
 $debug = '';
+$iterations = 0;
 $time_pre = microtime(true);
 $savedHashes = array();
-$iterations = 0;
 for($i=0;$i<9995278570;$i++) {
     if($foundthree == 1 && $i <= ($tempi + 1000)) {
         // Check the current hash for a pattern of 5
+        $md5hash = strtolower(md5($input.$i));
         if(!isset($savedHashes[$i])) {
-            $md5hash = strtolower(md5($input.$i));
-            $savedHashes[$i] = $md5hash;
+            for($h=0;$h<2016;$h++) {
+                $md5hash = strtolower(md5($md5hash));
+                $savedHashes[$i] = $md5hash;
+            }
         }
+         
         $debug = "checking 5 for $foundKey for hash $i - $md5hash<br>";
         preg_match('/('.$foundKey.')\1{4}/',$savedHashes[$i],$foundMatches);
         if(count($foundMatches)>0) {
@@ -43,8 +47,11 @@ for($i=0;$i<9995278570;$i++) {
     } elseif($foundthree == 0) {
         // Check the current hash for a pattern of 3
         if(!isset($savedHashes[$i])) {
-            $md5hash = strtolower(md5($input.$i));
-            $savedHashes[$i] = $md5hash;
+        $md5hash = strtolower(md5($input.$i));
+            for($h=0;$h<2016;$h++) {
+                $md5hash = strtolower(md5($md5hash));                
+                $savedHashes[$i] = $md5hash;
+            }
         }
         preg_match('/(.)\1{2}/',$savedHashes[$i],$foundMatches);
         if(count($foundMatches)>0) {
@@ -63,9 +70,14 @@ for($i=0;$i<9995278570;$i++) {
     if($iterations % 10000 == 0) {
         $time_post = microtime(true);
         $exec_time = $time_post - $time_pre;
-        echo "Done iteration $iterations in $exec_time seconds<Br>";
+        for($e=0;$e<$tempi;$e++) {
+            unset($savedHashes[$e]);
+        }
+        echo "Done iteration $iterations in $exec_time seconds - currently at $i - saved hashes count - ".count($savedHashes)."<Br>";
         flush();
         ob_flush();
+        
+        
     }
     //echo $debug;
 //    if(count($keys)>0) {
